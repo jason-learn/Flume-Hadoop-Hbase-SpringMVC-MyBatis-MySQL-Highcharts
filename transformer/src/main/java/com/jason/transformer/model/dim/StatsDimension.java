e0a6c91c0540c3f22d20ab5ef2523f2527956010
Jason Zhang <workmail.jason@foxmail.com>
2018-02-11 13:20:24 +0800

    Add files via upload

---
+++ transformer/src/main/java/com/jason/transformer/model/dim/StatsDimension.java
@@ -0,0 +1,7 @@
+package com.jason.transformer.model.dim;
+
+import com.jason.transformer.model.dim.base.BaseDimension;
+
+public abstract class StatsDimension extends BaseDimension {
+    // nothing
+}
