b8fc4b2f40646df72bbf6e3d6900fe13bfc2871d
Jason Zhang <workmail.jason@foxmail.com>
2018-02-15 12:42:30 +0800

    Add files via upload

---
+++ transformer/src/main/java/com/jason/transformer/service/IDimensionConverter.java
@@ -0,0 +1,23 @@
+package com.jason.transformer.service;
+
+import java.io.IOException;
+
+import com.jason.transformer.model.dim.base.BaseDimension;
+
+/**
+ * 提供专门操作dimension表的接口
+ * 
+ * @author jason
+ *
+ */
+public interface IDimensionConverter {
+    /**
+     * 根据dimension的value值获取id<br/>
+     * 如果数据库中有，那么直接返回。如果没有，那么进行插入后返回新的id值
+     * 
+     * @param dimension
+     * @return
+     * @throws IOException
+     */
+    public int getDimensionIdByValue(BaseDimension dimension) throws IOException;
+}
